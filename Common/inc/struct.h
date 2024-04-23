dea1deba3e7a41997ce28a29baf1f5f35160c268
abraham-vijai <abrahamvijai03@gmail.com>
2024-04-23 02:38:06 -0400

    Created ClientInfo struct

--- Common/inc/struct.h
+++ Common/inc/struct.h
@@ -2,19 +2,21 @@
 #define STRUCT_H
 
 #define MAX_CLIENT_COUNT 10
+#define BUFFER_SIZE 256
 
+// Holds information about each client
 typedef struct ClientInfo
 {
+    int clientSocket;
+    char clientId[MAX_CLIENT_COUNT];
+    char clientAddress[BUFFER_SIZE];
+};
 
-} ClientInfo;
-
-
+// Holds overall client information
 typedef struct MasterList
 {
     int numOfClients;
     ClientInfo clientElements[MAX_CLIENT_COUNT];
 } MasterList;
-
-
 
 #endif

503794e24c6c626288f104d8fe3ab91276aee46e
abraham-vijai <abrahamvijai03@gmail.com>
2024-04-23 23:09:54 -0400

    Parsed the command line args

--- chat-client/src/client.c
+++ chat-client/src/client.c
@@ -7,6 +7,7 @@ int initSharedMemory(MasterList **masterList);
 int main(int argc, char *argv[])
 {
     MasterList *masterList;
+    char args[2][BUFFER_SIZE];
 
     // Initialize log file
     logInit(CLIENT_LOG_FP);
@@ -24,6 +25,9 @@ int main(int argc, char *argv[])
         printf ("chat-client -user<userID> -server<server name>\n");
         return 0;
     }
+
+    // Parse the command line arguments
+    parseArguments(argc, argv, args);
     
     return 0;
 }
@@ -51,4 +55,26 @@ int initSharedMemory(MasterList **masterList)
     }
 
     return 0;
+}
+
+void parseArguments(int argc, char *argv[], char args[2][BUFFER_SIZE])
+{
+    // Loop through the command line arguments
+    for (int i = 0; i < argc; i++)
+    {
+        // Check if the argument starts with '-user'
+        if(strncmp(argv[i],"-user",strlen("-user")) == 0)
+        {
+            // Extract the user id
+            strncpy(args[0], argv[i] + strlen("-user"), BUFFER_SIZE);
+            args[0][strlen(args[0])] = '\0';
+        }
+        // Check if the argument starts with "-server"
+        else if (strncmp(argv[i], "-server", strlen("-server")) == 0)
+        {
+            // Extract the server name
+            strncpy(args[1], argv[i] + strlen("-server"), BUFFER_SIZE);
+            args[1][strlen(args[1])] = '\0'; 
+        } 
+    }
 }

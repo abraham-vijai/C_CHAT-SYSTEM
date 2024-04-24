53cb31a5a8b1c1f04847b4113dfbf77fa37c99c8
abraham-vijai <abrahamvijai03@gmail.com>
2024-04-23 23:00:35 -0400

    Initialized shared memory to store the master list

--- chat-client/inc/client.h
+++ chat-client/inc/client.h
@@ -2,8 +2,12 @@
 #define CLIENT_H
 
 #include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 #define CLIENT_LOG_FP "./chat-client/tmp/client.log"
 #define INIT_MESSAGE "==================== CLIENT STARTED ====================="
+#define SHM_KEY 16535
+#define ERROR -1
 
 #endif
--- chat-client/src/client.c
+++ chat-client/src/client.c
@@ -1,5 +1,8 @@
 #include "../inc/client.h"
 #include "../../Common/inc/struct.h"
+#include "../../Common/src/logger.c"
+
+int initSharedMemory(MasterList **masterList);
 
 int main()
 {
@@ -8,5 +11,37 @@ int main()
     // Initialize log file
     logInit(CLIENT_LOG_FP);
     logEvent(INIT_MESSAGE);
+    
+    logEvent("Intializing shared memory ...");
+    if(initSharedMemory(&masterList) == ERROR)
+    {
+        return ERROR;
+    }
+
+    return 0;
+}
+
+int initSharedMemory(MasterList **masterList)
+{
+    int shmID;
+    key_t shmKey;
+
+    shmKey = ftok("a", SHM_KEY);
+
+    // Attempt to get shared memory block
+    while((shmID = shmget(shmKey, sizeof(MasterList), 0) == ERROR))
+    {
+        logEvent("ERROR: Cannot get shared memory block");
+        return ERROR;
+    }
+
+    // Attach to shared memory block
+    *masterList = (MasterList*) shmat(shmID, NULL, 0);
+    if((*masterList) == NULL)
+    {
+        logEvent("ERROR: Cannot get shaerd memory address");
+        return ERROR;
+    }
+
     return 0;
 }
